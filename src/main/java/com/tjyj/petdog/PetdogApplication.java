4595bf21d2b83d9c7226a9e3627675c6dff1e01b
Taejoong <xownd6235@naver.com>
2021-12-21 16:12:43 +0900

    Create project

---
+++ src/main/java/com/tjyj/petdog/PetdogApplication.java
@@ -0,0 +1,13 @@
+package com.tjyj.petdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetdogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tjyj/petdog/PetdogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjyj.petdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetdogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
